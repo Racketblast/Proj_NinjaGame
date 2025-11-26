9ff03a1878b8e14e91297d7cbaa796f7fd542bd8
Racketblast <144691527+Racketblast@users.noreply.github.com>
2025-11-26 18:07:30 +0100

    TargetAI controller

--- Source/Proj_NinjaGame/TargetAIController.cpp
+++ Source/Proj_NinjaGame/TargetAIController.cpp
@@ -3,3 +3,106 @@
 
 #include "TargetAIController.h"
 
+ATargetAIController::ATargetAIController()
+{
+}
+
+void ATargetAIController::OnHeardSound(FVector SoundLocation)
+{
+}
+
+void ATargetAIController::RefreshChaseTarget()
+{
+}
+
+void ATargetAIController::StartChasingFromExternalOrder(FVector LastSpottedPlayerLocation)
+{
+}
+
+void ATargetAIController::AssignMission(ETargetMission NewMission, FVector MissionLocation)
+{
+}
+
+void ATargetAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+}
+
+void ATargetAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void ATargetAIController::OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result)
+{
+	Super::OnMoveCompleted(RequestID, Result);
+}
+
+void ATargetAIController::MoveToNextPatrolPoint()
+{
+}
+
+void ATargetAIController::StartChasing()
+{
+}
+
+void ATargetAIController::StopChasing()
+{
+}
+
+void ATargetAIController::StartAlert()
+{
+}
+
+void ATargetAIController::OnUnPossess()
+{
+	Super::OnUnPossess();
+}
+
+void ATargetAIController::HandleSuspiciousLocation(FVector Location)
+{
+}
+
+void ATargetAIController::CompleteMission()
+{
+}
+
+void ATargetAIController::StartMissionMoveTo(FVector Location)
+{
+}
+
+void ATargetAIController::StartSmoothRotationTowards(const FVector& TargetLocation, float RotationSpeed)
+{
+}
+
+void ATargetAIController::RunChaseFailsafe(float DeltaSeconds)
+{
+}
+
+void ATargetAIController::ResetSoundFlag()
+{
+}
+
+void ATargetAIController::OnAlertTimerExpired()
+{
+}
+
+void ATargetAIController::RetryMoveToNextPatrolPoint()
+{
+}
+
+void ATargetAIController::BeginSearch()
+{
+}
+
+void ATargetAIController::LookAround()
+{
+}
+
+void ATargetAIController::EndSearch()
+{
+}
+
+void ATargetAIController::OnTargetLost()
+{
+}
